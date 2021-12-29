848e50a88f8fe7ef838288529fe708d46ce5c555
Nick Curie <nick.curie64@gmail.com>
2021-12-28 22:12:04 -0800

    Early coin tracking application

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import Header from './components/Header'
+import Button from './components/Button'
+import Segment from './components/Segment'
+
 
 function App() {
+
+  // Save Coins
+  const saveCoins = (coins) => {
+    //console.log(coins)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header title='Welcome to Coinpurse'/>
+      <Segment title='Coin Tracker' onSave={saveCoins} />
     </div>
   );
 }
